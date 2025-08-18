14285f0b68b4cda3728401d6b203ee55a15209bb
gustavoDev02 <guga19italo@gmail.com>
2025-08-18 20:22:45 -0300

    feat: criando o header

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+export function Header(){
+    return(
+        <>
+            <header>
+                <h1>seja bem ao TrackFIt</h1>
+                <h2>o melhor app de treino</h2>
+            </header>
+        </>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client"; // dom = document object model
 import App from "./App.tsx";
+import { Header } from "./components/header.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Header/>
   </StrictMode>
 );
